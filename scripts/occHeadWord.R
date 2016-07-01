b3bc59a12591439572000a727245974fa0007229
rlzijdeman <richard.zijdeman@iisg.nl>
2016-07-01 08:50:06 +0200

    minor improvements in head word selection

--- scripts/occHeadWord.R
+++ scripts/occHeadWord.R
@@ -2,10 +2,10 @@
 # clear workspace
 rm(list = ls())
 
-
 # load libraries (use install.packages('package') first, if not installed yet)
 library(dplyr)
 library(tidyr)
+library(stringr)
 
 # setting working directory
 
@@ -27,16 +27,87 @@ df2$occSplit <- strsplit(df2$standard, " ") # seperate words
 
 # remove add words
 df2$occSplit <- lapply(df2$occSplit, function(x) {
-    badvec <- c("de", "het", "een", "van", "bij", "uit", "voor", "niet",
+    badvec <- c("de", "het", "een", "van", "bij", "uit", "voor", "niet", "op",
                 "eerste", "tweede", "derde", "vierde", "vijfde", 
-                "beroepstitel", "vermeld", "interpreteerbaar", "overgenomen")
+                "beroepstitel", "vermeld", "interpreteerbaar", "overgenomen",
+                "adjunct", "secretarie", "machine", "oost-indisch",
+                "beroepen", "gepensioneerd", "onbezoldigd", "oud", "semi",
+                "tijdelijk", "zonder", "plaatsvervangend", "gegageerd",
+                "los", "leerling", "privé", "privaat", "technisch", 
+                "provinciaal", "provincie")
     sapply(x, function(y) {
         ifelse(y %in% badvec, y <- NA, y)
     })
 })
 
-# get the first word
-df2$head <- sapply(df2$occSplit, "[[", 1)
+View(df2)
+
+### MORE ISSUES TO SOLVE
+# - delete if first word is "bedrijfstitel:"
+comp <- function(x) {
+    first <- sapply(x, "[[", 1)[1]
+    sapply(x, function(y) {
+        ifelse(first == "bedrijfstitel:",
+               y <- NA, y) 
+    })
+    
+}
+df2$occSplit2 <- sapply(df2$occSplit, comp)
+
+df2[df2$standard %in% "bedrijfstitel", ]
+
+
+
+# - eigen werk doende
+own_work <- function(x) {
+    first <- sapply(x, "[[", 1)[1]
+    second <- sapply(x, "[[", 1)[2]
+    third <- sapply(x, "[[", 1)[3]
+    ifelse(first == "eigen" & second == "werk" & third == "doende" ,
+           x <- NA, x)
+}
+df2$occSplit <- sapply(df2$occSplit, own_work)
+
+
+# - sergeant majoor (should be one word)
+sgt_mjr <- function(x) {
+    first <- sapply(x, "[[", 1)[1]
+    second <- sapply(x, "[[", 1)[2]
+    ifelse(first == "sergeant" & second == "majoor",
+     "sergeant majoor", x)
+}
+df2$occSplit <- sapply(df2$occSplit, sgt_mjr)
+
+# - if the first word contains a '-', use last word
+#dash <- function(x) {
+#    first <- sapply(x, "[[", 1)[1]
+#    last <- word(x, -1)
+#    ifelse(grepl("-", first), last, x)
+#}
+#df2$occSplit <- sapply(df2$standard, dash) #NB: now using standard, bc
+
+
+# - if more than 1 word and first word == 'meester' drop first word
+#
+#master <- function(x) {
+#    first <- sapply(x, "[[", 1)[1]
+#    count <- sapply(gregexpr("\\W+", x), length) + 1
+#    ifelse(count > 1  & first == "meester", 
+#           sapply(x, "[[", 1)[1] <- NA, x)
+#}
+#
+#df2$occSplit2 <- sapply(df2$standard, master) #NB: now using standard, bc
+
+
+
+
+# get the head word (first if not missing, else second)
+select_head <- function(x) {
+        first <- sapply(x, "[[", 1)[1]
+        second <- sapply(x, "[[", 1)[2]
+        ifelse(is.na(first), second, first)
+}
+df2$head <- sapply(df2$occSplit, select_head)
 
 
 # removing any spaces
